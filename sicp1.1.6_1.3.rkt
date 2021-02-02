88b3cc8003d548b93ce64102edf87972fec86cf8
ichise.masashi <ichise.masashi@gmail.com>
2021-02-02 22:39:02 +0900

    finish 1.1.6

--- sicp1.1.6_1.3.rkt
+++ sicp1.1.6_1.3.rkt
@@ -14,8 +14,15 @@
 ;        (else (squareSum y z))))
 
 (define (sumOfLargestTwoSquared x y z)
-  (cond ((and (< x y) (< x z))
+  (cond ((and (< x y) (< x z)) ; xが最小
          (squareSum y z))
-        ((and (< y x) (< y z))
+        ((and (< y x) (< y z)) ; yが最小
          (squareSum x z))
         (else (squareSum x y))))
+
+(sumOfLargestTwoSquared 1 2 3)
+;=> 13
+(sumOfLargestTwoSquared 3 2 1)
+;=> 13
+(sumOfLargestTwoSquared 2 3 1)
+;=> 13
