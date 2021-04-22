fb257ee5a480f4386fa46dee5e8b9837a7a96dc0
ichisemasashi <ichise.masashi@gmail.com>
2021-04-22 21:28:36 +0900

    練習問題 2.8 二つの区間の差

---
+++ ch02/sicp2.1.4_2.8.rkt
@@ -0,0 +1,7 @@
+; 練習問題 2.8 二つの区間の差
+(load "sicp2.1.4.rkt")
+(load "sicp2.1.4_2.7.rkt")
+
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (lower-bound y))
+                    (- (upper-bound x) (upper-bound y))))
