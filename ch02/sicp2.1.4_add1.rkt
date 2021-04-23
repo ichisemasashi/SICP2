952350402b24bdb85be852f398fe19ae4accfd1e
ichisemasashi <ichise.masashi@gmail.com>
2021-04-23 22:10:18 +0900

    2.1.4 add1

---
+++ ch02/sicp2.1.4_add1.rkt
@@ -0,0 +1,9 @@
+; SICP 2.1.4 add1
+;ほしいのは中央値と許容誤差で表される数値を扱うプログラムだ
+;例えば、3.5 ± 0.15のような区間
+(define (make-center-width c w)
+  (make-interval (- c w) (+ c w)))
+(define (center i)
+  (/ (+ (lower-bound i) (upper-bound i)) 2.0))
+(define (width i)
+  (/ (- (lower-bound i) (upper-bound i)) 2.0))
