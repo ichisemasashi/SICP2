2f6a9987a15702e4b0ba21015e36cd2a56ee5ac8
ichisemasashi <ichise.masashi@gmail.com>
2021-04-23 20:57:12 +0900

    練習問題 2.10 ゼロをまたぐ区間で割った場合

---
+++ ch02/sicp2.1.4_2.10.rkt
@@ -0,0 +1,15 @@
+; 練習問題 2.10 ゼロをまたぐ区間で割った場合にどうなるかはっきりしていない
+;この条件についてチェックして、もしこれが起こればエラーのシグナルを送るようにせよ。
+(load "sicp2.1.4_2.8.rkt")
+; 元のdiv-interval
+;(define (div-interval x y)
+;  (mul-interval x (make-interval (/ 1.0 (upper-bound y))
+;                                       (/ 1.0 (lower-bound y)))))
+(define (div-interval x y)
+  (let ((l (lower-bound y))
+         (u (upper-bound y)))
+    (if (<= (* l u) 0)
+       (error "Division error (interval spans 0)." y)
+       (mul-interval
+          x
+          (make-interval (/ 1.0 u) (/ 1.0 l))))))
