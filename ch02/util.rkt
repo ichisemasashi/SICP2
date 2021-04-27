265fff18930938aa04a1409481cbfb2ba1ae8721
ichisemasashi <ichise.masashi@gmail.com>
2021-04-27 20:31:57 +0900

    2.3.3 add1 マップのネスト

--- ch02/util.rkt
+++ ch02/util.rkt
@@ -14,3 +14,17 @@
   (if (= count 0)
       b
       (fib-iter (+ a b) a (- count 1))))
+
+(define (prime? n)
+  (if (= 1 n) false
+     (= n (smallest-divisor n))))
+(define (smallest-divisor n)
+  (find-divisor n 2))
+(define (find-divisor n test-divisor)
+  (cond ((< n (square test-divisor)) n)
+        ; もし n が素数でないならば、それは√n 以下の約数を持つ
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+(define (divides? a b)
+  (= (remainder b a) 0))
+
