da7d5bcc717cb71bf357fa9684248f741f65c7a3
ichisemasashi <ichise.masashi@gmail.com>
2021-08-17 20:51:54 +0900

    util.rkt

--- ch02/util.rkt
+++ ch02/util.rkt
@@ -72,3 +72,12 @@
          ((predicate (car seq))
           (cons (car seq) (filter predicate (cdr seq))))
          (else (filter predicate (cdr seq)))))
+
+(define (attach-tag type-tag contents)
+  (cons type-tag contents))
+(define (type-tag datum)
+  (if (pair? datum) (car datum)
+      (error "Bad tagged datum: TYPE-TAG" datum)))
+(define (contents datum)
+  (if (pair? datum) (cdr datum)
+      (error "Bad tagged datum: CONTENTS" datum)))
