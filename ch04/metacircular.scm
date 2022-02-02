272dfdab0b5797fa7fbef97c23abe5c044cf396c
ichisemasashi <ichise.masashi@gmail.com>
2022-02-02 21:33:37 +0900

    [wip][jp] meta-circular

--- ch04/metacircular.scm
+++ ch04/metacircular.scm
@@ -299,16 +299,90 @@
                 (frame-values frame)))))
   (env-loop env))
 
+;; 指定した環境の中で変数を新しい値に設定する
+(define (set-variable-value! var val env)
+  (define (env-loop env)
+    (define (scan vars vals)
+      (cond ((null? vars)
+             (env-loop (enclosing-environment env)))
+            ((eq? var (car vars)) (set-car! vals val))
+            (else (scan (cdr vars) (cdr vals)))))
+    (if (eq? env the-empty-environment)
+        (error "Unbound variable: SET!" var)
+        (let ((frame (first-frame env)))
+          (scan (frame-variables frame)
+                (frame-values frame)))))
+  (env-loop env))
 
+;; 変数を定義する
+(define (define-variable! var val env)
+  (let ((frame (first-frame env)))
+    (define (scan vars vals)
+      (cond ((null? vars)
+              (add-binding-to-frame! var val frame))
+            ((eq? var (car vars)) (set-car! vals val))
+            (else (scan (cdr vars) (cdr vals)))))
+    (scan (frame-variables frame) (frame-values frame))))
 
+;;;;;;;;;;;;;;;;; 4.1.4 評価器をプログラムとして実行する
+(define (setup-environment)
+  (let ((initial-env
+    (extend-environment
+      (primitive-procedure-names)
+      (primitive-procedure-objects)
+      the-empty-environment)))
+  (define-variable! 'true true initial-env)
+  (define-variable! 'false false initial-env)
+  initial-env))
+(define the-global-environment (setup-environment))
+;; 基本手続きを表現する方法
+(define (primitive-procedure? proc)
+  (tagged-list? proc 'primitive))
+(define (primitive-implementation proc) (cadr proc))
+;; setup-environment
+;; 基本手続きの名前と実装手続きをリストから取ります。
+(define primitive-procedures
+  (list (list 'car car)
+        (list 'cdr cdr)
+        (list 'cons cons)
+        (list 'null? null?)
+        ⟨more primitives⟩ ))
+(define (primitive-procedure-names)
+  (map car primitive-procedures))
+(define (primitive-procedure-objects)
+  (map (lambda (proc) (list 'primitive (cadr proc)))
+       primitive-procedures))
 
+;; 基本手続きを適用する
+(define (apply-primitive-procedure proc args)
+  (apply-in-underlying-scheme
+    (primitive-implementation proc) args))
+;; 対話型評価環境 (REPL) をモデル化したドライバループ 
+(define input-prompt ";;; M-Eval input:")
+(define output-prompt ";;; M-Eval value:")
+(define (driver-loop)
+  (prompt-for-input input-prompt)
+  (let ((input (read)))
+    (let ((output (eval input the-global-environment)))
+       (announce-output output-prompt)
+       (user-print output)))
+  (driver-loop))
+(define (prompt-for-input string)
+  (newline) (newline) (display string) (newline))
+(define (announce-output string)
+  (newline) (display string) (newline))
 
+;; 特別な表示手続きを使い、複合手続きの環境部分を表示しないようにしています
+(define (user-print object)
+  (if (compound-procedure? object)
+      (display (list 'compound-procedure
+                     (procedure-parameters object)
+                     (procedure-body object)
+                     '<procedure-env>))
+      (display object)))
 
-
-
-
-
-
-
+;; 後はグローバル環境を初期化してドライバループを開始するだけで、 評価器を実行できます。
+;; (define the-global-environment (setup-environment))
+;; (driver-loop)
 
 
